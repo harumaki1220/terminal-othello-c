2690ffa524b0831b8f0f5d10fefc01ce5992e471
harumaki1220 <haruma181220@gmail.com>
2026-01-13 13:51:57 +0900

    feat: 盤面表示機能の実装（2次元配列と2重ループ）

--- main.c
+++ main.c
@@ -2,6 +2,27 @@
 
 int main(void)
 {
-    printf("Hello Othello\n");
+    int board[8][8] = {0};
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            if (board[i][j] == 0)
+            {
+                printf(".");
+            }
+            else if (board[i][j] == 1)
+            {
+                printf("B");
+            }
+            else if (board[i][j] == 2)
+            {
+                printf("W");
+            }
+        }
+        printf("\n");
+    }
+
     return 0;
 }
