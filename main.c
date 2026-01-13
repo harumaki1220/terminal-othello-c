604486d7adce48a6314af0db949fb7bc9cdc0707
harumaki1220 <haruma181220@gmail.com>
2026-01-14 00:33:39 +0900

    feat: 入力値のバリデーションと石の配置処理を追加

--- main.c
+++ main.c
@@ -34,11 +34,34 @@ void print_board(int board[8][8])
 int main(void)
 {
     int board[8][8] = {EMPTY};
+    int row, col;
 
     board[3][3] = WHITE;
     board[3][4] = BLACK;
     board[4][3] = BLACK;
     board[4][4] = WHITE;
+
+    print_board(board);
+
+    printf("どこに置きますか？ (行 列): ");
+    scanf("%d %d", &row, &col);
+
+    if (row >= 0 && row <= 7 && col >= 0 && col <= 7)
+    {
+        if (board[row][col] == EMPTY)
+        {
+            board[row][col] = BLACK;
+            printf("%d行 %d列に置きました。\n", row, col);
+        }
+        else
+        {
+            printf("そこにはすでに石があります！\n");
+        }
+    }
+    else
+    {
+        printf("範囲外の入力（0〜7以外）です。正しい値を入力してください。\n");
+    }
 
     print_board(board);
 
