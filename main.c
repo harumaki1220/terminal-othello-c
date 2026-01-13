bb228dd028e658788717799a1f023743eeeb31b4
harumaki1220 <haruma181220@gmail.com>
2026-01-13 16:33:04 +0900

    feat: 盤面に座標ガイドを表示する機能を追加

--- main.c
+++ main.c
@@ -6,8 +6,12 @@
 
 void print_board(int board[8][8])
 {
+    printf("  0 1 2 3 4 5 6 7\n");
+
     for (int i = 0; i < 8; i++)
     {
+        printf("%d ", i);
+
         for (int j = 0; j < 8; j++)
         {
             if (board[i][j] == EMPTY)
